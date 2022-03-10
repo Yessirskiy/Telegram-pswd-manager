d0d6460d605c0bddc57a3604ec539959a0b75e5e
KolyaDobrydnev <nidobrydnev@gmail.com>
2022-03-10 19:27:17 +0300

    Updating to work with Manager class. newProfile and allProfiles functions added

--- database.py
+++ database.py
@@ -1,8 +1,57 @@
+from typing import Tuple, Dict, List
 import config
 import sqlite3
 
-#def newProfile(service_name: str, )
 
+def allProfiles(database: Tuple[sqlite3.Connection, sqlite3.Cursor]):
+    '''
+    Returns all the profiles from DB
+
+    Args:
+        database (tuple[sqlite3.Connection, sqlite3.Cursor]): connection and cursor of DB
+    
+    Returns:
+        profiles (List[Dict]) | Literal[False]: List of dictionaries with profiles 
+            {service_name : (username_hash, password_hash)}
+    '''
+    
+    try:
+        profiles = []
+        con, cur = database
+        fetching_query = '''SELECT * FROM profiles'''
+        cur.execute(fetching_query)
+        data = cur.fetchall()
+        for profile in data:
+            profile_id, service, username_hash, password_hash = profile
+            profiles.append({service : (username_hash, password_hash)})
+        return profiles
+    except sqlite3.Error as e:
+        print(e)
+        return False
+    
+
+def newProfile(service_name: str, username_hash: str, password_hash: str, database: Tuple[sqlite3.Connection, sqlite3.Cursor]) -> bool:
+    '''
+    Creates new profile in DB
+
+    Args:
+        service_name (str): Name of the profile's service
+        username_hash (str): Hashed username of the profile's service
+        password_hash (str): Hashed password of the profile's service
+        database (tuple[sqlite3.Connection, sqlite3.Cursor]): connection and cursor of DB
+    
+    Returns:
+        result (bool): Success of operation
+    '''
+    try:
+        con, cur = database
+        insert_query = '''INSERT INTO profiles(service, username, password) VALUES(?, ?, ?)'''
+        cur.execute(insert_query, (service_name, username_hash, password_hash,))
+        con.commit()
+    except sqlite3.Error as e:
+        print(e)
+        return False
+    return True
 
 def setUp():
     '''     
@@ -16,6 +65,13 @@ def setUp():
     try:
         con = sqlite3.connect(database=config.DB_NAME)
         cur = con.cursor()
+        init_query = '''CREATE TABLE IF NOT EXISTS profiles (
+            id integer PRIMARY KEY,
+            service text NOT NULL,
+            username text,
+            password text
+        );'''
+        cur.execute(init_query)
     except sqlite3.Error as e:
         print(e)
         return False
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import config
 from telebot import TeleBot, types
 import database as db
+from Manager import Manager
+import time
 
 bot = TeleBot(token=config.BOT_TOKEN, parse_mode='Markdown') # Initializing Telegram Bot
+mng = Manager()
 
 @bot.message_handler(commands=['start'])
 def greetings(message):
@@ -17,17 +20,26 @@ def greetings(message):
         manage_master_btn = types.KeyboardButton(text= 'Manage Master Account 🔑')
         main_menu_markup.add(add_profile_btn, edit_profile_btn, delete_profile_btn, export_profiles_btn, manage_master_btn)
         bot.send_message(message.chat.id, text='_Hello, use buttons from menu below to manage your passwords :)_', reply_markup=main_menu_markup)
-    
+
 @bot.message_handler(content_types=['text'])
 def handle_menu(message):
     pass
  
 def main():
     database = db.setUp()
-    if database:
-                
-
-        bot.polling()
+    if database: 
+        profiles = db.allProfiles(database=database)
+        if profiles != False:
+            mng.profiles = profiles
+            print("Bot succesfully launched")
+            while True:
+                try:
+                    bot.infinity_polling()
+                except Exception as e:
+                    print(e)
+                    time.sleep(3)
+        else:
+            print("Couldn't parse profiles from DB")
     else:
         print("Couldn't connect to DB")
 
